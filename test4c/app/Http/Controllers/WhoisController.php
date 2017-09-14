1dac5f97a0acbeb6e6da5e17d18ab2f6c7809225
yiqiaoxihui <yiqiaoxihui@163.com>
2017-09-14 00:11:36 -0700

    add auto query to whois

--- test4c/app/Http/Controllers/WhoisController.php
+++ test4c/app/Http/Controllers/WhoisController.php
@@ -13,6 +13,61 @@ class WhoisController extends Controller
     	echo $count;
         return view('whois', ['input' => '']);
     }
+    public function ip_n_to_ip($ip,$ipn){
+      $elements=explode(".", $ip);
+      $len=count($elements);
+      if($len==1){
+        $ip=$ip.'.0.0.0';
+      }elseif($len==2) {
+        $ip=$ip.'.0.0';
+      }elseif($len==3) {
+        $ip=$ip.'.0';
+      }else{
+
+      }
+      //echo "</br>--1---";
+      //print_r($elements);
+      //echo "</br>";
+      print_r($ip);
+      if (intval($ipn)<32 & intval($ipn)>0){
+        #print ip_num[1]
+        $ip_begin="";
+        $ip_end="";
+        //qu zheng
+        $ip_int=floor(intval($ipn)/8);
+        $ip_rem=intval($ipn)%8;
+        for($i=0;$i<$ip_int;$i++){
+          $ip_begin=$ip_begin.$elements[$i].'.';
+          $ip_end=$ip_end.$elements[$i].'.';
+        }
+        //echo "</br>--2----".(string)$ip_int;
+        //print_r($ip_begin);
+        $ip_begin=$ip_begin."".(string)(intval($elements[$ip_int]) & (~((1<<(8-$ip_rem))-1)));
+        $ip_end=$ip_end."".(string)(intval($elements[$ip_int])|((1<<(8-$ip_rem))-1));
+        if ($ip_int<3){
+          for($i=$ip_int+1;$i<4;$i++){
+            $ip_begin=$ip_begin.'.0';
+            $ip_end=$ip_end.'.255';
+          }
+        }
+        //echo "</br>--3----";
+        //print_r($ip_begin);
+        $result[]=$ip_begin;
+        $result[]=$ip_end;
+        return $result;
+      }
+      elseif(intval($ipn)==32){
+        $result[]=$ip;
+        $result[]=$ip;
+        return $result;
+      }
+      else{
+        $result[]='0.0.0.0';
+        $result[]='0.0.0.0';
+        return $result;
+      }
+    }
+
     public function store()
     {
         if(isset($_POST['from'])){
@@ -44,11 +99,19 @@ class WhoisController extends Controller
                 }
               }
               if ($_POST['search'] == 'ip'){
+                $ip=$_POST['content'];
+                $ip_n = bindec(decbin(ip2long($ip)));
                 $total = Whois::where('ip_begin', '<=', $ip_n)->where('ip_end', '>=', $ip_n)->count();
+                if($total<=0){
+                  WhoisController::query_now($ip);
+                  //$ip_n = bindec(decbin(ip2long($ip)));
+                  $rows = Whois::where('ip_begin', '<=', $ip_n)->where('ip_end', '>=', $ip_n)->skip($params->offset)->take($params->limit)->get();
+                }
               }
               else{
                 $total = Whois::where('content', 'like', '%'.$_POST['content'].'%')->count();
               }
+
             }
             else{
               if(isset($params->sort)){
@@ -79,6 +142,97 @@ class WhoisController extends Controller
             $data = array('message' => $msg);
           }
           return $data;
+        }
+    }
+    public function query_now($ip){
+        $command="whois ".$ip;
+        //$result=shell_exec($command);
+        //echo ($command);
+        exec($command,$arr);
+        //print_r($arr);
+        $data="";
+        foreach ($arr as $key => $value) {
+          if($value==''){
+            $data=$data."\n";
+          }
+          if(substr($value,0,1)=="%" or substr($value,0,1)=="#"){
+            continue;
+          }
+          if(substr($value,0,6)=="route:"){
+            break;
+          }
+          $data=$data."\n".$value;
+        }
+
+        $data=preg_replace("/\n{3,}/","\n\n",$data);
+        $data=preg_replace("/ {2,}/","",$data);
+        $data=trim($data);
+
+        preg_match("/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) {0,1}- {0,1}(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/",$data, $ip);
+        //print_r(count($ip));
+        if(count($ip)>=3){
+          $ip_begin=bindec(decbin(ip2long($ip[1])));
+          $ip_end=bindec(decbin(ip2long($ip[2])));
+          $hash=md5($data);
+          //print_r($ip);
+          $whois=new Whois;
+          $whois->ip_begin=$ip_begin;
+          $whois->ip_end=$ip_end;
+          $whois->content=$data;
+          $whois->hash=$hash;
+          $whois->save();
+        }else{
+          //x.x.x.x/n
+          preg_match("/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})/",$data, $ip);
+          if(count($ip)>=3){
+            //print_r($ip);
+            $ip1=ip_n_to_ip($ip[1],$ip[2]);
+            $ip_begin=bindec(decbin(ip2long($ip1[1])));
+            $ip_end=bindec(decbin(ip2long($ip1[2])));
+            $hash=md5($data);
+            $whois=new Whois;
+            $whois->ip_begin=$ip_begin;
+            $whois->ip_end=$ip_end;
+            $whois->content=$data;
+            $whois->hash=$hash;
+            $whois->save();
+            //print_r($ip1);
+          }else{
+            //x.x.x/n
+            preg_match("/inetnum:(\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})/",$data, $ip);
+            if(count($ip)>=3){
+              //print_r($ip);
+              $ip2=ip_n_to_ip($ip[1],$ip[2]);
+              $ip_begin=bindec(decbin(ip2long($ip2[1])));
+              $ip_end=bindec(decbin(ip2long($ip2[2])));
+              $hash=md5($data);
+              $whois=new Whois;
+              $whois->ip_begin=$ip_begin;
+              $whois->ip_end=$ip_end;
+              $whois->content=$data;
+              $whois->hash=$hash;
+              $whois->save();
+              //print_r($ip2);
+            }else{
+              //x.x/n
+              preg_match("/inetnum:(\d{1,3}\.\d{1,3})\/(\d{1,2})/",$data, $ip);
+              if(count($ip)>=3){
+                $ip3=ip_n_to_ip($ip[1],$ip[2]);
+                $ip_begin=bindec(decbin(ip2long($ip3[1])));
+                $ip_end=bindec(decbin(ip2long($ip3[2])));
+                $hash=md5($data);
+                $whois=new Whois;
+                $whois->ip_begin=$ip_begin;
+                $whois->ip_end=$ip_end;
+                $whois->content=$data;
+                $whois->hash=$hash;
+                $whois->save();
+                //print_r($ip3);
+              }else{
+                //print_r("no respect query data!");
+              }
+            }
+          }
         }
     }
 }
