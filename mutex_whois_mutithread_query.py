fded3925b73c0404fb9e30d3dc592d1868cc5b31
yiqiaoxihui <yiqiaoxihui@163.com>
2017-09-23 20:44:10 -0700

    add listern key to stop

--- mutex_whois_mutithread_query.py
+++ mutex_whois_mutithread_query.py
@@ -11,12 +11,25 @@ import json
 import threading
 
 mutex=threading.Lock()
-
+end_thread_signal=0
+ip_list=[]
+w_fp=0
+wl_fp=0
 def whois_query(begin,end):
+	#global end_thread_signal,w_fp,wl_fp,ip_list
 	count=0
 	left_ip=[]
 	print "query"+str(begin)+'~'+str(end)+"start!"
 	for i in range(begin-1,end):
+		print str(begin)+'~'+str(end)+":stop!"
+		if(end_thread_signal==1):
+			for j in range(i,end):
+				if mutex.acquire(True):
+					wl_fp.write(ip_list[j])
+					wl_fp.write('\n')
+					#w_fp.flush()
+					mutex.release()
+			break
 		print str(begin)+'~'+str(end)+":"+str(i)+'ip:'+ip_list[i]
 		arg = 'whois '+ip_list[i];
 		query_result=os.popen(arg)
@@ -24,8 +37,6 @@ def whois_query(begin,end):
 		for line in query_result:
 			if (line[0]=='%' or line[0]=='#'):    	#delete unnecessary info
 				continue
-			if(line[:6]=="route:"):
-				break
 			data=data+line
 		data=re.sub("\n{3,}","\n\n",data)
 		data=re.sub(" {2,}", " ", data)
@@ -47,49 +58,81 @@ def whois_query(begin,end):
 			mutex.release()
 	#deal the left ip which is limit query
 	while len(left_ip)>0:
-		print str(begin)+'~'+str(end)+":"+"last left:"+str(len(left_ip))+"current ip:"+left_ip[0]
-		arg = 'whois '+left_ip[0]
-		query_result=os.popen(arg)
-		data=""
-		for line in query_result:
-			if (line[0]=='%' or line[0]=='#'):    	#delete unnecessary info
-				continue
-			data=data+line
-		data=re.sub("\n{3,}","\n\n",data)
-		data=re.sub(" {2,}", " ", data)
-		data=data.strip()							#delete whitespace in head or tail
-		data=data.replace("\n","\\n")
-		#print data	
-		if len(data)==0 or data=="Query rate limit exceeded":
-			#print str(begin)+'~'+str(end)+":left_ip query fail"+"current ip:"+left_ip[0]
-			left_ip.append(left_ip[0])
-			del left_ip[0]
-			continue
-		if "'" in data:
-			data='{"content":"'+data+'","ip":"'+left_ip[0]+'"}'
+		if(end_thread_signal==1):
+			for line in left_ip:
+				if mutex.acquire(True):
+					wl_fp.write(line)
+					wl_fp.write('\n')
+					#w_fp.flush()
+					mutex.release()
+			break
 		else:
-			data="{'content':'"+data+"','ip':'"+left_ip[0]+"'}"
-		if mutex.acquire(True):
-			w_fp.write(data)
-			w_fp.write('\n')
-			#w_fp.flush()
-			mutex.release()
-		del left_ip[0]
-	#v_fp.close()
+			print str(begin)+'~'+str(end)+":"+"last left:"+str(len(left_ip))+"current ip:"+left_ip[0]
+			arg = 'whois '+left_ip[0]
+			query_result=os.popen(arg)
+			data=""
+			for line in query_result:
+				if (line[0]=='%' or line[0]=='#'):    	#delete unnecessary info
+					continue
+				data=data+line
+			data=re.sub("\n{3,}","\n\n",data)
+			data=re.sub(" {2,}", " ", data)
+			data=data.strip()							#delete whitespace in head or tail
+			data=data.replace("\n","\\n")
+			#print data	
+			if len(data)==0 or data=="Query rate limit exceeded":
+				#print str(begin)+'~'+str(end)+":left_ip query fail"+"current ip:"+left_ip[0]
+				left_ip.append(left_ip[0])
+				del left_ip[0]
+				continue
+			if "'" in data:
+				data='{"content":"'+data+'","ip":"'+left_ip[0]+'"}'
+			else:
+				data="{'content':'"+data+"','ip':'"+left_ip[0]+"'}"
+			if mutex.acquire(True):
+				w_fp.write(data)
+				w_fp.write('\n')
+				#w_fp.flush()
+				mutex.release()
+			del left_ip[0]	
+
 	print "query"+str(begin)+'~'+str(end)+"completed!"
+def listen_key():
+	global end_thread_signal
+	while(True):
+		print "linsten_key:"
+		key=raw_input()
+		print "key:"+key
+		if 'stop' in key:
+			end_thread_signal=1
+			print "end_thread_signal:"+str(end_thread_signal)
+			break
 
 def main():
 	if __name__=='__main__':
+		#end_thread_signal=0
 		thread_count=10
 		i=0
-		data=""
-		global ip_list,w_fp
+		global ip_list,w_fp,wl_fp,end_thread_signal
 		#global ip_list
-		ip_fp=open('/data/all_neighbor_ip.txt','r')
-		path="/data/whois_all_result_8724"
-		w_fp=open(path,'w')
+		end_thread_signal=0
+		ip_path=raw_input("please input ip file path:")
+		ip_in_row=input("please input the row of ip of the file(0~N):")
+		result_path=raw_input("please input result path:")
+		ip_left_path=raw_input("please input ip left path:")
+		thread_count=input("please input the thread number:")
+		result_path="/data/whois_all_result_8724"
+		ip_path='/data/all_neighbor_ip.txt'
+		ip_left_path='/data/8724_left'
+		ip_in_row=0
+		ip_fp=open(ip_path,'r')
+		wl_fp=open(ip_left_path,'a')
+		w_fp=open(result_path,'a')
 		ip_list=ip_fp.readlines()
 		ip_fp.close()
+		ip_count=len(ip_list)
+		print ip_count
+		ip_list=list(set(ip_list))
 		ip_count=len(ip_list)
 		print ip_count
 		current=0
@@ -105,11 +148,13 @@ def main():
 				parts[num].append(current+1)
 				parts[num].append(ip_count)
 		#preprocessing ip
+
 		for i in range(0,ip_count):
+			ip_list[i]=ip_list[i].split()[ip_in_row]
 			ip_list[i]=ip_list[i].strip()
 			#ip_list[i]=re.sub("\d*\/\d*","1",ip_list[i])
-		ip_list=list(set(ip_list))
-			#print ip_list[i]
+			print ip_list[i]
+		print len(ip_list)
 		query_threads=[]
 		#create multithread
 		for i in range(0,thread_count):
@@ -119,11 +164,13 @@ def main():
 		for t in query_threads:
 			t.setDaemon(False)		#
 			t.start()
+		threading.Thread(target=listen_key).start()
 		for t in query_threads:
 			t.join()
 		w_fp.close()
+		wl_fp.close
 		print "all query completed!"
-
+		print len(ip_list)
 		#for num in range(0,thread_count):
 		#	print str(parts[num][0])+'~'+str(parts[num][1])
 main()
