e7a2e4bd70aefff7e1bd1bbfd1e48319d93677ee
yiqiaoxihui <yiqiaoxihui@163.com>
2017-10-30 08:44:28 -0700

    creat_tree

--- test1.py
+++ test1.py
@@ -1,6 +1,20 @@
-for i in range(0,100):
-	print i
-	for j in range(100,200):
-		if i==33:
-			break;break
-			
+import socket
+import struct
+f=open('../liuyang','w')
+f.write('sdf%s' % '123')
+f.write('asd')
+f.close()
+number=(10<<24)+(255<<2)+2
+str1='r010_123'
+print "aaaaaa"+str(str1.find('dr010_'))
+#print socket.inet_ntoa(struct.pack('!L', number))
+def test():
+	global a
+	a=1
+
+	def in_test():
+		global a
+		a=a+1
+	in_test()
+	print a
+test()
